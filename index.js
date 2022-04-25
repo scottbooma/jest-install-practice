db8c159c4ef57d7059db1fcb9a95745a5cd89120
scottbooma <scottbooma@gmail.com>
2022-04-25 13:58:49 -0400

    Delete files

--- index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    multiply: function (number1, number2) {
-        return number1 * number2
-    },
-
-    add: function (number1, number2) {
-        return number1 + number2
-    },
-}
